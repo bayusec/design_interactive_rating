b4e03aef1506f97141cd23205d04b87d66afe29c
BayuDev <29111275+BatouDkoder@users.noreply.github.com>
2023-01-07 02:54:05 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <InteractiveRating />
+  </div>
+</template>
+
+<script setup>
+import InteractiveRating from "@/components/InteractiveRating.vue";
+</script>
+
+<style>
+body {
+  background: hsl(216, 12%, 8%);
+}
+</style>
